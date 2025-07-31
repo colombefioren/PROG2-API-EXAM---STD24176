00965c6fe1b1ed1a31bfd4dc43804100fc052f7f
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-31 08:18:11 +0300

    feat: q1-3 answers

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ from starlette.responses import Response
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return Response(content=json.dumps({"message":"Hello there!"}),status_code=200, media_type="text/html")
+# Q1
+
+@app.get("/ping")
+def get_ping():
+    return Response(content="pong",status_code=200)
+
+# Q2
+
+@app.get("/home")
+def get_home():
+    with open("welcome.html","r",encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content,status_code=200,media_type="text/html")
+
+# Q3
+
+
+@app.get("/{full_path:path}")
+def catch_all(full_path: str):
+    with open("not_found.html","r",encoding="utf-8") as file:
+        html_content=file.read()
+    return Response(content=html_content,status_code=404,media_type="text/html")
